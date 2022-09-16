51e9c78f3581c7bf4462f1e6c98c3e674cad3fdf
IsuruHL <isurugodakandahl@gmail.com>
2022-09-17 03:20:26 +0530

    Add yarn.lock and mockServiceWorker

--- public/mockServiceWorker.js
+++ public/mockServiceWorker.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (0.47.2).
+ * Mock Service Worker (0.47.3).
  * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
@@ -70,7 +70,7 @@ self.addEventListener('message', async function (event) {
     case 'CLIENT_CLOSED': {
       activeClientIds.delete(clientId)
 
-      const remainingClients = allClients.filter(client => {
+      const remainingClients = allClients.filter((client) => {
         return client.id !== clientId
       })
 
@@ -115,7 +115,7 @@ self.addEventListener('fetch', function (event) {
   const requestId = Math.random().toString(16).slice(2)
 
   event.respondWith(
-    handleRequest(event, requestId).catch(error => {
+    handleRequest(event, requestId).catch((error) => {
       if (error.name === 'NetworkError') {
         console.warn(
           '[MSW] Successfully emulated a network error for the "%s %s" request.',
@@ -155,7 +155,8 @@ async function handleRequest(event, requestId) {
           ok: clonedResponse.ok,
           status: clonedResponse.status,
           statusText: clonedResponse.statusText,
-          body: clonedResponse.body === null ? null : await clonedResponse.text(),
+          body:
+            clonedResponse.body === null ? null : await clonedResponse.text(),
           headers: Object.fromEntries(clonedResponse.headers.entries()),
           redirected: clonedResponse.redirected,
         },
@@ -182,11 +183,11 @@ async function resolveMainClient(event) {
   })
 
   return allClients
-    .filter(client => {
+    .filter((client) => {
       // Get only those clients that are currently visible.
       return client.visibilityState === 'visible'
     })
-    .find(client => {
+    .find((client) => {
       // Find the client ID that's recorded in the
       // set of clients that have registered the worker.
       return activeClientIds.has(client.id)
@@ -278,7 +279,7 @@ function sendToClient(client, message) {
   return new Promise((resolve, reject) => {
     const channel = new MessageChannel()
 
-    channel.port1.onmessage = event => {
+    channel.port1.onmessage = (event) => {
       if (event.data && event.data.error) {
         return reject(event.data.error)
       }
@@ -291,7 +292,7 @@ function sendToClient(client, message) {
 }
 
 function sleep(timeMs) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, timeMs)
   })
 }
